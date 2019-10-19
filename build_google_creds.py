72fe287358ce8f1268e29fb1e9b556e2faa7a09b
Immanuel George <immanuel.george@vertical-knowledge.com>
2019-10-19 16:23:46 -0400

    .replace('\\n', '\n'),

--- build_google_creds.py
+++ build_google_creds.py
@@ -5,7 +5,7 @@ google_creds = {
     "type": os.environ.get("GOOGLE_FIREBASE_TYPE"),
     "project_id": os.environ.get("GOOGLE_FIREBASE_PROJECT_ID"),
     "private_key_id": os.environ.get("GOOGLE_FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.environ.get("GOOGLE_FIREBASE_PRIVATE_KEY").replace("\\\\n", "\n"),
+    "private_key": os.environ.get("GOOGLE_FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
     "client_email": os.environ.get("GOOGLE_FIREBASE_CLIENT_EMAIL"),
     "client_id": os.environ.get("GOOGLE_FIREBASE_CLIENT_ID"),
     "auth_uri": os.environ.get("GOOGLE_FIREBASE_AUTH_URI"),
